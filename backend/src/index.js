854c9564afd56db9993b86656de033cf6e8c1326
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-05-03 10:56:08 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+
+
+import { connectDB } from "./lib/db.js";
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 4003; // Set the port to the environment variable PORT or 4003
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); // Log the port number to the console
+    console.log(`http://localhost:${port}`); // Log the URL to the console
+    connectDB();
+});
